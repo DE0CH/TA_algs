d948a88b09ba360723911eea9336a2e4e5a35808
Deyao Chen <chendeyao000@gmail.com>
2024-06-18 16:10:17 +0100

    update play

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,6 +1,10 @@
+use itertools::izip;
+
 fn main() {
-    let mut v: Vec<i32> = Vec::with_capacity(10);
-    unsafe {v.set_len(10)};
-    v[9] = 10;
-    println!("{:?}", v);
+    let mut yp_sn: Vec<_> = vec![0;5];
+    let yp = vec![0;5];
+    let x = vec![0;5];
+    for (x, yp_sn, _yp) in izip!(x, yp_sn.iter_mut(), yp) {
+        *yp_sn = x;
+    }
 }
