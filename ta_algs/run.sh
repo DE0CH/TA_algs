373668a32f62109e87953cf39eecbbccde77d0fc
Deyao Chen <chendeyao000@gmail.com>
2024-06-18 16:10:09 +0100

    update run

--- ta_algs/run.sh
+++ ta_algs/run.sh
@@ -7,13 +7,7 @@ N=$(echo $FIRST_LINE | cut -d ' ' -f 2)
 GD=$(echo $FIRST_LINE | cut -d ' ' -f 3)
 echo "N=$N, D=$D"
 cargo build --release
-make -C ../
-cargo flamegraph --release --bin=delta -- $D $N <(tail -n +2 $FILE)
-perf record -F 99 -g -- ../TA_improved_delta $D $N <(tail -n +2 $FILE)
-perf script > out.perf
-../../FlameGraph/stackcollapse-perf.pl out.perf > out.folded
-../../FlameGraph/flamegraph.pl out.folded > flamegraph_c.svg
+export RUST_BACKTRACE=1
 time ./target/release/delta $D $N <(tail -n +2 $FILE)
 time ../TA_improved_delta $D $N <(tail -n +2 $FILE)
-
 echo "ground truth $GD"
