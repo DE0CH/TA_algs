3fd39a9d34fe02afb8b771a538c33f13f9bcfacf
Deyao Chen <chendeyao000@gmail.com>
2024-06-19 15:53:34 +0100

    update run

--- ta_algs/run.sh
+++ ta_algs/run.sh
@@ -8,6 +8,9 @@ GD=$(echo $FIRST_LINE | cut -d ' ' -f 3)
 echo "N=$N, D=$D"
 cargo build --release
 export RUST_BACKTRACE=1
-time ./target/release/delta $D $N <(tail -n +2 $FILE)
+time ./target/release/bardelta $D $N <(tail -n +2 $FILE) --seed $RANDOM
+time ../TA_improved_bardelta $D $N <(tail -n +2 $FILE)
+time ./target/release/delta $D $N <(tail -n +2 $FILE) --seed $RANDOM
 time ../TA_improved_delta $D $N <(tail -n +2 $FILE)
+
 echo "ground truth $GD"
