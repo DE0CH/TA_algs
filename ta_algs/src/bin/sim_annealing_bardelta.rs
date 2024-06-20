1a098b61d227c1edeeb427cad7cd399f121b1ecd
Deyao Chen <chendeyao000@gmail.com>
2024-06-20 13:10:46 +0100

    improve sim annealing efficiency

--- ta_algs/src/bin/sim_annealing_bardelta.rs
+++ ta_algs/src/bin/sim_annealing_bardelta.rs
@@ -6,6 +6,7 @@ fn temperature(current: usize, max: usize) -> NotNan<f64> {
     let current = current as f64;
     let max = max as f64;
     let t = 1.0 - (current + 1.0) / max;
+    let t = t * 0.02;
     NotNan::new(t).unwrap()
 }
 
@@ -20,21 +21,16 @@ fn pp(current: NotNan<f64>, new: NotNan<f64>, t: NotNan<f64>) -> NotNan::<f64> {
     let p = (new - current) / t;
     NotNan::new(p.exp()).unwrap()
 }
-// Let s = s0
-// For k = 0 through kmax (exclusive):
-// T ← temperature( 1 - (k+1)/kmax )
-// Pick a random neighbour, snew ← neighbour(s)
-// If P(E(s), E(snew), T) ≥ random(0, 1):
-// s ← snew
-// Output: the final state s
 fn main() {
     let (points, seed, iterations) = get_raw_points();
     let points_grid = PointsGrid::new(points);
     let mut rng = fastrand::Rng::with_seed(seed);
     let xc = points_grid.generate_random_point(&mut rng);
     let mut xc = xc.round_down();
+    let xc_sn = xc.snap_down();
 
-    let mut global = xc.get_bardelta();
+    let mut global = xc_sn.get_bardelta();
+    let mut current = xc_sn.get_bardelta();
 
     let total_iterations = iterations as usize;
     for current_iteration in 0..total_iterations {
@@ -43,11 +39,14 @@ fn main() {
         let mc = points_grid.get_mc(current_iteration, total_iterations);
         let y = xc.generate_neighbor_point(&k, mc, &mut rng);
         let ym = y.round_down();
-        global = global.max(ym.get_bardelta());
+        let ym_sn = ym.snap_down();
+        let new_current = ym_sn.get_bardelta();
+        global = global.max(new_current);
 
-        if pp(xc.get_bardelta(), ym.get_bardelta(), t) >= NotNan::new(rng.f64()).unwrap() {
+        if pp(current, new_current, t) >= NotNan::new(rng.f64()).unwrap() {
             xc = ym;
         }
+        current = new_current;
     }
     println!("{}", global)
 }
