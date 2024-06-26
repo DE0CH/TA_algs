077df210ccf598192e32fdd05fc31b6e9d7c8410
Deyao Chen <chendeyao000@gmail.com>
2024-06-26 14:55:53 +0100

    update play

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,10 +1,28 @@
-use itertools::izip;
+#![feature(layout_for_ptr)]
+use std::{alloc::Layout, ops::Drop};
+
+trait Help {}
+
+#[derive(Debug)]
+struct Me {
+    data: Vec<String>
+}
+
+impl Drop for Me {
+    fn drop(&mut self) {
+        println!("Dropping Me");
+    }
+}
+
+impl Help for Me {}
 
 fn main() {
-    let mut yp_sn: Vec<_> = vec![0;5];
-    let yp = vec![0;5];
-    let x = vec![0;5];
-    for (x, yp_sn, _yp) in izip!(x, yp_sn.iter_mut(), yp) {
-        *yp_sn = x;
-    }
+    let me = Me { data: vec!["please".to_string(), "help".to_string()] };
+    let me: Box<dyn Help> = Box::new(me);
+    let p = Box::into_raw(me) as *mut Me;
+    let inner = unsafe {
+        std::mem::transmute::<Box::<dyn Help>, Box::<Me>>(me)
+    };
+    dbg!(&inner.data);
+    assert_eq!(inner.data, vec!["hello", "world"]);
 }
