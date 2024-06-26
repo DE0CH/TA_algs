b677ef132de02647c90c96a09a4230dac721240e
Deyao Chen <chendeyao000@gmail.com>
2024-06-26 14:57:46 +0100

    update run

--- ta_algs/run.sh
+++ ta_algs/run.sh
@@ -8,9 +8,9 @@ GD=$(echo $FIRST_LINE | cut -d ' ' -f 3)
 echo "N=$N, D=$D"
 cargo build --release
 export RUST_BACKTRACE=1
-time ./target/release/bardelta $D $N <(tail -n +2 $FILE) --seed $RANDOM
-time ../TA_improved_bardelta $D $N <(tail -n +2 $FILE)
-time ./target/release/delta $D $N <(tail -n +2 $FILE) --seed $RANDOM
-time ../TA_improved_delta $D $N <(tail -n +2 $FILE)
 
-echo "ground truth $GD"
+make -C ../
+cargo run --release --bin=sim_annealing_bardelta -- -i 100000 $D $N <(tail -n +2 $FILE)
+# valgrind --leak-check=full --num-callers=20 --tool=memcheck
+../TA_improved_bardelta -iter 100000 $D $N <(tail -n +2 $FILE)
+../sim_bardelta -iter 100000 $D $N <(tail -n +2 $FILE)
