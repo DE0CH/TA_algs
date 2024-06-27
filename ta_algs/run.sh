04e7746ee46ba8a2c7f3c5bc494da262be34f179
Deyao Chen <chendeyao000@gmail.com>
2024-06-27 22:11:21 +0100

    update run script

--- ta_algs/run.sh
+++ ta_algs/run.sh
@@ -9,11 +9,6 @@ echo "N=$N, D=$D"
 
 
 make -C ../ 
-# time ../TA_improved_delta -iter 100000 $D $N <(tail -n +2 $FILE)
-# valgrind --leak-check=full \
-#          --show-leak-kinds=all \
-#          --track-origins=yes \
-#          --verbose \
-#          --log-file=valgrind-out.txt \
+../TA_improved_delta -iter 100000 $D $N <(tail -n +2 $FILE)
 ../TA_improved_bardelta -iter 100000 $D $N <(tail -n +2 $FILE)
 echo "Ground truth $GD"
