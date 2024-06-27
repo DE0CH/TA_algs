1886a415b821d38b7367d529b2ff5eb92242c637
Deyao Chen <chendeyao000@gmail.com>
2024-06-27 14:20:03 +0100

    update run

--- ta_algs/run.sh
+++ ta_algs/run.sh
@@ -6,11 +6,8 @@ D=$(echo $FIRST_LINE | cut -d ' ' -f 1)
 N=$(echo $FIRST_LINE | cut -d ' ' -f 2)
 GD=$(echo $FIRST_LINE | cut -d ' ' -f 3)
 echo "N=$N, D=$D"
-cargo build --release
-export RUST_BACKTRACE=1
 
-make -C ../
-cargo run --release --bin=sim_annealing_bardelta -- -i 100000 $D $N <(tail -n +2 $FILE)
-# valgrind --leak-check=full --num-callers=20 --tool=memcheck
-../TA_improved_bardelta -iter 100000 $D $N <(tail -n +2 $FILE)
-../sim_bardelta -iter 100000 $D $N <(tail -n +2 $FILE)
+
+make -C ../ 
+time ../TA_improved_bardelta -iter 100000 $D $N <(tail -n +2 $FILE)
+time ../TA_improved_delta -iter 100000 $D $N <(tail -n +2 $FILE)
